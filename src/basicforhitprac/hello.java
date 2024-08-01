aa1d8358dc1d1192217bf5190cd39fb8a05f54d5
saiStiffy <saikrn1997@gmail.com>
2024-08-01 08:42:33 +0400

    agian new commer coming

--- src/basicforhitprac/hello.java
+++ src/basicforhitprac/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 System.out.println("new commer sending");
+System.out.println("again new commer coming");
 	}
 
 }
